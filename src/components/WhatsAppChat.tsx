093e888dec09133fa1f7dd8649725b9554e2ad2f
Dhruvil vyas <158256201+ruvilvyas@users.noreply.github.com>
2025-06-19 08:18:07 +0530

    whatsapp update

--- src/components/WhatsAppChat.tsx
+++ src/components/WhatsAppChat.tsx
@@ -4,7 +4,7 @@ import { MessageCircle } from "lucide-react";
 export default function WhatsAppChat() {
   return (
     <a
-      href="https://wa.me/918160977869?text=Hi%20Guruji%20SMSA%2C%20I'm%20interested%20in%20your%20video%20editing%20services."
+      href="https://wa.me/919979089031?text=Hi%20Gleamira%2C%20I'm%20interested%20in%20your%20jewellery%20collections."
       className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg flex items-center justify-center transition-all duration-300"
       target="_blank"
       rel="noopener noreferrer"
